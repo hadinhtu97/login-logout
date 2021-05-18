4a9f1090059f875ae3f3064f41d33dbd8f2d5930
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-05-18 15:31:05 +0700

    Check someone sign in or not

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
 
+import {signIn, signOut, useSession} from 'next-auth/client'
+
+
 const Home = () => {
+
+    const [session, loading] = useSession()
+
     return (
         <>
             <Head>
                 <title>Next temp</title>
             </Head>
             <main className={styles.main}>
-
+                {
+                    !session && <>
+                        Not signed in <br/>
+                        <button onClick={() => signIn()}>Sign in</button>
+                    </>
+                }
+                {
+                    session && <>
+                        Signed in as {session.user.email} <br/>
+                        <button onClick={()=>signOut()}>Sign Out</button>
+                    </>
+                }
             </main>
         </>
     )
