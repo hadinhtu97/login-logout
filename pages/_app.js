ad83c3cec53ff169a234840c74de6bc5810f18b8
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-05-18 15:31:32 +0700

    Share session between pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import {Providers} from 'next-auth/providers'
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <Providers session={pageProps.session}>
+            <Component {...pageProps} />
+        </Providers>
+    )
 }
 
 export default MyApp
